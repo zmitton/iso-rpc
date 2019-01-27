d61ca5c209ada7240a4a8a0e05762532202cfa25
Zac Mitton <zacmitton22@gmail.com>
2019-01-26 22:16:22 -0400

    init

---
+++ index.js
@@ -0,0 +1,36 @@
+const fetch = require('isomorphic-fetch')
+
+class EthRpc{
+  constructor(provider = "http://localhost:8545", fetchOptions = {}){
+    this.provider = provider
+  }
+  static get(obj,prop){
+    if(prop in obj){
+      return obj[prop]
+    }else{
+      return async(...params) => {
+        let connection = await fetch(obj.provider, Object.assign({
+          method: "POST",
+          headers: {"Content-Type": "application/json"},
+          body: JSON.stringify({
+            method:prop,
+            params:params,
+            id:obj.id++
+          })
+        },obj.fetchOptions)).catch()
+
+        let response = await connection.json()
+
+        if (response.error) {
+          throw new Error(response.error.message)
+        }else{
+          return response.result
+        }
+      }
+    }
+  }
+}
+
+module.exports = function Rpc(provider, fetchOptions){
+  return new Proxy(new EthRpc(provider, fetchOptions),EthRpc)
+}
